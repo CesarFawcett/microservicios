1db591186573cb730dc0867dc0300b7a7e5cb7ed
Cesar David Fawcett Perozo <31809162+CesarFawcett@users.noreply.github.com>
2025-03-27 11:11:47 -0500

    Add files via upload

---
+++ microservices/src/test/java/edu/unimag/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unimag.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
