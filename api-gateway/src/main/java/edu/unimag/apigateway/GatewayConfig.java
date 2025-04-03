c284e37f2cc24e662802d0dd650164d47d4338b4
Cesar Fawcett <Cesarfawcett20@gmail.com>
2025-04-02 21:06:51 -0500

    actualizacion 3

--- api-gateway/src/main/java/edu/unimag/apigateway/GatewayConfig.java
+++ api-gateway/src/main/java/edu/unimag/apigateway/GatewayConfig.java
@@ -1,34 +1,24 @@
+package edu.unimag.apigateway;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class GatewayRouteConfig {
+public class GatewayConfig {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                // Ruta para inventory-service
                 .route("inventory-service", r -> r.path("/api/inventory/**")
-                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://inventory-service"))
-
-                // Ruta para order-service
                 .route("order-service", r -> r.path("/api/order/**")
-                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://order-service"))
-
-                // Ruta para payment-service
                 .route("payment-service", r -> r.path("/api/payment/**")
-                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://payment-service"))
-
-                // Ruta para product-service
                 .route("product-service", r -> r.path("/api/product/**")
-                        .filters(f -> f.stripPrefix(1))
                         .uri("lb://product-service"))
-
                 .build();
     }
 }
