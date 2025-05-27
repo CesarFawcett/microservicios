c8162f243e88692d1785867e05ef227718626709
Cesar Fawcett <Cesarfawcett20@gmail.com>
2025-05-27 17:47:53 -0500

    actualizacion entidades

--- Product/src/main/java/edu/unimag/product/ProductApplication.java
+++ Product/src/main/java/edu/unimag/product/ProductApplication.java
@@ -3,9 +3,11 @@ package edu.unimag.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
 public class ProductApplication {
 
